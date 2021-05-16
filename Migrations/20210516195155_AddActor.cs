47950ff098961dfed57e96eb0a7854c890cce53b
Giovanna Gregório Guimarães <giovanna_gregorio@univem.edu.br>
2021-05-16 16:54:03 -0300

    Migration e scaffolf

---
+++ Migrations/20210516195155_AddActor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace movie2.Migrations
+{
+    public partial class AddActor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(maxLength: 60, nullable: false),
+                    IdMovie = table.Column<int>(nullable: false),
+                    MovieId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actor", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Actor_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Actor_MovieId",
+                table: "Actor",
+                column: "MovieId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actor");
+        }
+    }
+}
